1598981a014aad378c3679428c108e8406d9ca35
eshanama <77765506+eshanama@users.noreply.github.com>
2022-10-21 01:09:31 -0400

    finished random num generator

--- montecarlosim.py
+++ montecarlosim.py
@@ -12,10 +12,23 @@ c = 3517
 K = 2**17
 
 # linear congruential random number generator
-def random_num(i):
+def xi_equation(i):
     if i == 0:
         return ((a)*(x_0)+c) % K
     else:
-        return random_num(i-1)
+        return ((a)*xi_equation(i-1)+c) % K
 
-print(random_num(1))
+def random_num(n):
+    num = xi_equation(n)
+    return num / K
+
+# checking first three random numbers
+print("u_0: " + str(random_num(0)))     # should be 0.4195
+print("u_1: " + str(random_num(1)))     # should be 0.0425
+print("u_2: " + str(random_num(2)) + "\n")     # should be 0.1274
+
+print("u_51: " + str(random_num(51)))
+print("u_52: " + str(random_num(52)))
+print("u_53: " + str(random_num(53)))
+
+# Section 2: Random Variable Generator -------------------------------------------------------------------
