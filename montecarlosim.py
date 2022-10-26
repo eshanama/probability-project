472c1e829ce7d2111e80608aae1e29b418818c1e
eshanama <77765506+eshanama@users.noreply.github.com>
2022-10-26 13:28:50 -0400

    added simulation

--- montecarlosim.py
+++ montecarlosim.py
@@ -26,16 +26,7 @@ def random_num(n):
     num = xi_random_num(n)
     return num / K
 
-# checking first three random numbers
-print("u_0: " + str(random_num(0)))             # should be 0.4195
-print("u_1: " + str(random_num(1)))             # should be 0.0425
-print("u_2: " + str(random_num(2)) + "\n")      # should be 0.1274
-
-print("u_51: " + str(random_num(51)))
-print("u_52: " + str(random_num(52)))
-print("u_53: " + str(random_num(53)) + "\n")
-
-# Section 2: Random Variable Generator -------------------------------------------------------------------
+# Section 2: Random Variable Generator --------------------------------------------------------------------------------
 # 2a: Discrete Random Variable
 
 def xi_discrete_rv(t):
@@ -49,17 +40,40 @@ def discrete_rv_generator(u):
         x+=1
     return x-1
 
-print(random_num(3))
-print("")
-
-print(xi_discrete_rv(64))
-print(xi_discrete_rv(65))
-print(xi_discrete_rv(66))
-
-print("")
-print(discrete_rv_generator(random_num(3)))
-
 # 2b: Continuous Random Variable
 def xi_continuous_rv(u):
     Ft = -12 * np.log(1-u)
     return Ft
+
+# Section 3: Mathematical Model ---------------------------------------------------------------------------------------
+
+def simulation(run_num):
+    w = 0
+    num_calls = 1
+    random_number = random_num(run_num)
+
+    while True:
+        if random_number < 0.2:
+            w += 10
+            random_number = random_number / 0.2
+            if num_calls >= 4:
+                return w
+        elif random_number > 0.2 and random_number < 0.5:
+            w += 32
+            random_number = (random_number - 0.2) / 0.3
+            if num_calls >= 4:
+                return w
+        else:
+            Ft = xi_continuous_rv(random_number)
+            if Ft > 25:
+                w += 32
+                random_number = (random_number - 0.5) / 0.5
+                if num_calls >= 4:
+                    return w
+                else:
+                    w += 6 + Ft
+                    return w
+        num_calls += 1
+
+print(random_num(1))
+print(simulation(1))
